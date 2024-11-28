1cb3d45c372872f4ca477579c2b4ecba9a7ba6b5
github kadokichi <dydy2929@yahoo.co.jp>
2024-11-28 20:29:39 +0900

    クリック時の処理

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
   const title = 'Vue.js Lesson'
   let price = 9.99
+  function increment() {
+    price += 1
+    console.log(price)
+  }
 </script>
 
 <template>
   <h1>Title: {{ title }}</h1>
-  <h2>Price:$ {{ price - 1 }}</h2>
+  <h2>Price: ${{ price - 1 }}</h2>
+  <button @click="increment">button</button>
 </template>
 <style>
   h1 {
