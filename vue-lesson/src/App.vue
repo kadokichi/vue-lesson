21317a71f71657592c4eb38fccde8859c5f81344
github kadokichi <dydy2929@yahoo.co.jp>
2024-11-28 21:32:20 +0900

    reactive()

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -1,16 +1,33 @@
 <script setup>
-  import { ref } from 'vue'
+  import { ref, reactive } from 'vue'
+
   const title = ref('Vue.js Lesson')
+
   let price = ref(9.99)
   function increment() {
     price.value += 1
+    instructor.age += 1
+    instructor.bio = 'hai dy'
+    instructor.sns.x = 'hello'
     //console.log(price.value)
   }
+
   const info = ref({
     students: 1000,
     rating: 4
   })
   console.log(info.value.students)
+
+  const instructor = reactive({
+    name: 'dydy',
+    age: 25,
+    sns: {
+      x: "@_xcvb",
+      youtube: '@dydy'
+    }
+  })
+  instructor.bio = 'hello'
+  console.log(instructor.sns)
 </script>
 
 <template>
@@ -18,6 +35,9 @@
   <h2>Price: ${{ price - 1 }}</h2>
   <button @click="increment">button</button>
   <h2>Student: {{ info.students }}</h2>
+  <h2>Instructor age: {{ instructor.age }}</h2>
+  <h2>Instructor bio: {{ instructor.bio }}</h2>
+  <h2>Instructor SNS: {{ instructor.sns.x }}</h2>
 </template>
 <style>
   h1 {
