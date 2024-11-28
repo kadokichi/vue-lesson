58300c627b3d58a68db9eee7104df592b23ae5c0
github kadokichi <dydy2929@yahoo.co.jp>
2024-11-28 22:17:52 +0900

    reactive()の中でref()を使用する

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -4,11 +4,13 @@
   const title = ref('Vue.js Lesson')
 
   let price = ref(9.99)
+
   function increment() {
     price.value += 1
     instructor.age += 1
     instructor.bio = 'hai dy'
     instructor.sns.x = 'hello'
+    instructor.email = 'info@example.com'
     //console.log(price.value)
   }
 
@@ -24,10 +26,12 @@
     sns: {
       x: "@_xcvb",
       youtube: '@dydy'
-    }
+    },
+    email: ref('dydy@example.com')
   })
   instructor.bio = 'hello'
   console.log(instructor.sns)
+  console.log(instructor.email)
 </script>
 
 <template>
@@ -38,6 +42,7 @@
   <h2>Instructor age: {{ instructor.age }}</h2>
   <h2>Instructor bio: {{ instructor.bio }}</h2>
   <h2>Instructor SNS: {{ instructor.sns.x }}</h2>
+  <h2>Instructor email: {{ instructor.email }}</h2>
 </template>
 <style>
   h1 {
