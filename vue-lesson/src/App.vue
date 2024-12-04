67a8a96a005cab6815e77aab2fd9c127705440df
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-04 21:28:16 +0900

    ReactiveEffect

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -1,15 +1,26 @@
 <script setup>
   import { ref, computed } from 'vue'
   const score = ref(0)
+  const count =ref(0)
+  function tmp() {
+    return score.value > 3 ? 'Good' : 'Bad'
+  }
   const evaluation = computed(() => {
+    console.log('evaluation')
     return score.value > 3 ? 'Good' : 'Bad'
   })
+  function getEvalation() {
+    console.log(evaluation.value)
+  }
 </script>
 
 <template>
   <p>{{ score> 3 ? 'Good' : 'Bad' }}</p>
-  <p>{{ evaluation }}</p>
-  <p>{{ score }}</p>
-  <button @click="score++">+1</button>
+  <p>{{ tmp() }}</p>
+  <p>score: {{ score }}</p>
+  <p>count: {{ count }}</p>
+  <button @click="score++">+1 score</button>
+  <button @click="count++">+1 count</button>
+  <button @click="getEvalation">getEvalation</button>
 </template>
 
