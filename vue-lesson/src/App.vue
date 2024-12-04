6a84fa1708f1768701eb1c272a055632a4bcf9fc
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-04 23:11:35 +0900

    Watch

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -1,18 +1,26 @@
 <script setup>
-  import { ref, watchEffect } from 'vue'
-  const  count = ref(0)
-  watchEffect(() => {
-    console.log('watchEffect')
-    console.log(count.value)
-    setTimeout(() => {
-    console.log('after 1 second')
-  }, 1000)
-  count.value = 'hello'
+  import { ref, watchEffect, watch } from 'vue'
+  const count1 = ref(0)
+  const count2 = ref(0)
+  const count3 = ref(0)
+  watch([count1, count2], (newValue, oldValue) => {
+    console.log('watch')
+    console.log('newValue', newValue)
+    console.log('oldValue', oldValue)
+    //console.log(count1.value, count2.value, count3.value)
   })
+  // watchEffect(() => {
+  //   console.log('watchEffect')
+  //   console.log(count1.value, count2.value, count3.value)
+  // })
 </script>
 
 <template>
-  <p>{{ count }}</p>
-  <button @click="count++">+1</button>
+  <p>{{ count1 }}(count1)</p>
+  <p>{{ count2 }}(count2)</p>
+  <p>{{ count3 }}(count3)</p>
+  <button @click="count1++">count1++</button>
+  <button @click="count2++">count2++</button>
+  <button @click="count3++">count3++</button>
 </template>
 
