f38189d41b717a9ae03007b9bfd55e2947541c65
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-06 21:03:22 +0900

    v-for&オブジェクト

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -1,21 +1,14 @@
 <script setup>
   import { ref } from 'vue'
-  const fruits = ref([
-    {id: 1, name: 'Apple'},
-    {id: 2, name: 'Apple'},
-    {id: 3, name: 'Banana'},
-    {id: 4, name: 'Grape'}
-    ])
+  const user = ref({
+    name: 'dydy',
+    age: 25,
+    gender: 'mix'
+  })
 </script>
 
 <template>
-  <button @click="fruits.shift()">button</button>
-  <template v-for="({id, name}, index) in fruits" :key="id">
-    <template v-if="id === 2">
-      <p>id: {{ id }}</p>
-      <p><input type="text">{{ name }}({{ index }})</p>
-    </template>
-  </template>
+  <p v-for="(value, key, index) in user" :key="value">{{key}}: {{ value }}({{ index }})</p>
 </template>
 
 <style>
