cebaefa61a527366c024b3bc5f64e8f57df4b75a
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-06 20:57:57 +0900

    v-for&v-if

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -4,13 +4,18 @@
     {id: 1, name: 'Apple'},
     {id: 2, name: 'Apple'},
     {id: 3, name: 'Banana'},
-    {id: 1, name: 'Grape'}
+    {id: 4, name: 'Grape'}
     ])
 </script>
 
 <template>
   <button @click="fruits.shift()">button</button>
-  <li v-for="({id, name}, index) in fruits" :key="id"><input type="text">{{ name }}({{ index }})</li>
+  <template v-for="({id, name}, index) in fruits" :key="id">
+    <template v-if="id === 2">
+      <p>id: {{ id }}</p>
+      <p><input type="text">{{ name }}({{ index }})</p>
+    </template>
+  </template>
 </template>
 
 <style>
