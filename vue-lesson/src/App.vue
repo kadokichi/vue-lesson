eb109fd6b02ffd60941b89154f2a59f8f0a8f09a
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-06 20:17:58 +0900

    templateを使用したv-if

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
   import { ref } from 'vue'
   const ok = ref(true)
-  const maybeOk = ref(true)
 </script>
 
 <template>
   <button @click="ok = !ok">toggle</button>
-  <p v-if="ok">OK</p>
-  <p v-else-if="maybeOk">maybe OK</p>
-  <p v-else>NOT OK</p>
+  <template v-if="ok">
+    <p>OK</p>
+    <p>Hello</p>
+    <p>Hi</p>
+  </template>
 </template>
 
 <style>
