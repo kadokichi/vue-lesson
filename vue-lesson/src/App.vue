5fdf311bffb4245921e1e440af7e5b7136b4e8a8
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-10 21:12:52 +0900

    FoodChoiseApp

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -1,18 +1,48 @@
 <script setup>
-  import { ref } from 'vue'
-  //import CountUp from './components/CountUp.vue';
-  //import BaseButton from './components/BaseButton.vue';
-  //import ShowCount from './components/ShowCount.vue';
-  import ResetButton from './components/ResetButton.vue';
-  const count = ref(0)
+  import { ref } from 'vue';
+  import FoodCard from './components/FoodCard.vue';
+  const foods = ref([
+    {name: '寿司', emoji: '🍣'},
+    {name: 'ハンバーガー', emoji: '🍔'},
+    {name: 'ラーメン', emoji: '🍜'},
+    {name: '焼肉', emoji: '🍖'},
+    {name: 'ピザ', emoji: '🍕'}
+    ])
+  const pickedFoodIndex = ref(0)
+  const newFoodIndex = ref(1)
+  function pickFood(index) {
+    pickedFoodIndex.value = index
+    newFoodIndex.value++
+  }
 </script>
 
 <template>
-  <p>count {{ count }}</p>
-  <button @click="count++">+1</button>
-  <ResetButton @reset-count="count = $event"/>
+  <main>
+    <h1 class="title">今日何食べる？</h1>
+    <FoodCard
+      :name="foods[pickedFoodIndex].name"
+      :emoji="foods[pickedFoodIndex].emoji"
+      @click="pickFood(pickedFoodIndex)"
+    />
+    <template v-if="newFoodIndex < foods.length">
+      <p>VS</p>
+      <FoodCard
+        :name="foods[newFoodIndex].name"
+        :emoji="foods[newFoodIndex].emoji"
+        @click="pickFood(newFoodIndex)"
+      />
+    </template>
+  </main>
 </template>
 
-<style>
+<style scoped>
+  main {
+    width: 350px;
+    margin: 100px auto;
+    text-align: center;
+  }
 
+  .title {
+    margin-bottom: 60px;
+  }
 </style>
