c34dd14f0c7fb850d8560da7a890a1cd61c1952d
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-13 23:18:00 +0900

    動的コンポーネント

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -1,25 +1,17 @@
 <script setup>
-  import BaseCard from './components/BaseCard.vue';
+  import { ref } from 'vue';
+  import CompA from './components/CompA.vue';
+  import CompB from './components/CompB.vue';
+  import CompC from './components/CompC.vue';
+  const currentComp = ref(CompA)
 </script>
 
 <template>
-  <h1>Slots</h1>
-  <BaseCard v-slot="{ pageCount }">
-    <!-- <template #header="{ pageCount }"> -->
-      <p>PageCount: {{ pageCount }}</p>
-      <h2 v-if="pageCount === 1">Vue.js Course</h2>
-      <h2 v-if="pageCount === 2">HTML Course</h2>
-      <h2 v-if="pageCount === 3">CSS Course</h2>
-    <!-- </template> -->
-    <!-- <template #main="{ pageCount }"> -->
-      <p v-if="pageCount === 1">This is Vue.js Course</p>
-      <p v-if="pageCount === 2">This is HTML Course</p>
-      <p v-if="pageCount === 3">This is CSS Course</p>
-    <!-- </template> -->
-    <!-- <template #footer> -->
-      <p>Introductor: kdy</p>
-    <!-- </template> -->
-  </BaseCard>
+  <h1>Dynamic Components</h1>
+  <button @click="currentComp = CompA">A</button>
+  <button @click="currentComp = CompB">B</button>
+  <button @click="currentComp = CompC">C</button>
+  <component :is="currentComp" />
 </template>
 
 <style scoped>
