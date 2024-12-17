ea8b169593ccc0b2e4409662456aa3472ba5ebee
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-17 20:20:56 +0900

    input,textareaにv-modelを適用する

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -1,26 +1,17 @@
 <script setup>
-  import { defineAsyncComponent, shallowRef } from 'vue';
-  import CompA from './components/CompA.vue';
-  //import CompB from './components/CompB.vue';
-  import BaseLoader from './components/BaseLoader.vue';
-  import ErrorMessage from './components/ErrorMessage.vue';
-  const CompB = defineAsyncComponent({
-    loader: () => import('./components/CompB.vue'),
-    loadingComponent: BaseLoader,
-    delay: 200,
-    errorComponent: ErrorMessage,
-    timeout: 2000
-  })
-  import CompC from './components/CompC.vue';
-  const currentComp = shallowRef(CompA)
+import { ref } from 'vue';
+const userInput = ref('')
+const message = ref('')
 </script>
 
 <template>
-  <h1>Dynamic Components</h1>
-  <button @click="currentComp = CompA">A</button>
-  <button @click="currentComp = CompB">B</button>
-  <button @click="currentComp = CompC">C</button>
-  <component :is="currentComp" />
+  <h1>V-model</h1>
+  <h2>Text</h2>
+  <input v-model="userInput" type="text">
+  <p>{{ userInput }}</p>
+  <h2>Textarea</h2>
+  <textarea v-model="message"></textarea>
+  <p style="white-space: pre;">{{ message }}</p>
 </template>
 
 <style scoped>
