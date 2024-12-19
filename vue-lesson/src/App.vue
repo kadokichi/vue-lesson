740242e0adcb241d026cedf64f5e8e509eb82f77
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-19 22:50:12 +0900

    Transition

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -5,33 +5,29 @@
 
 <template>
   <h1>Animation</h1>
-  <div :class="{ 'opacity-80': isShow, 'opacity-20': !isShow }">Hello</div>
-  <button @click="isShow = !isShow">Show</button>
+  <button @click="isShow = !isShow">switch</button>
+  <Transition>
+    <div v-if="isShow">Hello</div>
+  </Transition>
 </template>
 
 <style scoped>
-  div {
+  .v-enter-from {
+    opacity: 0;
+  }
+  .v-enter-active {
     transition: opacity 1s;
-    animation: slide 1s;
   }
-
-  .opacity-80 {
-    opacity: 0.8;
+  .v-enter-to {
+    opacity: 1;
   }
-
-  .opacity-20 {
-    opacity: 0.2;
+  .v-leave-from {
+    opacity: 1;
   }
-
-  @keyframes slide {
-    0% {
-      transform: translateX(0);
-    }
-    50% {
-      transform: translateX(100px);
-    }
-    100% {
-      transform: translateX(0);
-    }
+  .v-leave-active {
+    transition: opacity 1s;
+  }
+  .v-leave-to {
+    opacity: 0;
   }
 </style>
