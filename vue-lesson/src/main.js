b0a0de4bee4ba8077ef4b76485f5ae3b847868ba
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-22 15:28:11 +0900

    Vue Routerインストール

--- vue-lesson/src/main.js
+++ vue-lesson/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue"
 import App from "./App.vue"
+import router from "./router"
 import BaseIcon from "./components/BaseIcon.vue"
 import './assets/main.css'
 import 'animate.css';
 
 const app = createApp(App)
+app.use(router)
 app.component('BaseIcon', BaseIcon)
 app.mount('#app')
