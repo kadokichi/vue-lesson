9b66a176ae0cb42ae3f0a3ae7d5aa359e5a53f09
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-26 21:06:08 +0900

    routeオブジェクトでURL情報を取得する

--- vue-lesson/src/views/BlogView.vue
+++ vue-lesson/src/views/BlogView.vue
@@ -1,3 +1,23 @@
+<script setup>
+  import { watch } from 'vue'
+  import { RouterLink, useRoute } from 'vue-router';
+  const route = useRoute()
+  console.log(route.params.id)
+  watch(
+    () => route.params.id,
+    () => {
+      console.log('watch')
+  })
+</script>
+
 <template>
-  <h2>Blog</h2>
+  <h2>Blog(id: {{ $route.params.id }})</h2>
+  <p>version: {{ $route.params.version }}</p>
+  <RouterLink
+    :to="{
+      name: 'blog',
+      params: { id: Number($route.params.id) + 1,
+      version: $route.params.version}
+    }"
+  >NEXT</RouterLink>
 </template>
