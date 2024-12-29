52ea5fb70c9db621492e18deb71022ddd8ab1bbb
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-29 10:36:01 +0900

    Piniaを使ってStoreを定義するdefineStore()

--- vue-lesson/src/main.js
+++ vue-lesson/src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
+import { createPinia } from "pinia"
 import BaseIcon from "./components/BaseIcon.vue"
 import './assets/main.css'
 import 'animate.css';
 
 const app = createApp(App)
-app.use(router)
+const pinia = createPinia()
+app.use(router, pinia)
 app.component('BaseIcon', BaseIcon)
 app.mount('#app')
