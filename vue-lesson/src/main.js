9b44cc5b3626855c4fe8ce6347d642de948dc0ab
github kadokichi <dydy2929@yahoo.co.jp>
2024-12-29 15:27:27 +0900

    コンポーネント以外のところでStoreで定義したデータを使う方法

--- vue-lesson/src/main.js
+++ vue-lesson/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
 import { createPinia } from "pinia"
+import { useCounterStore } from './stores/counter';
 import BaseIcon from "./components/BaseIcon.vue"
 import './assets/main.css'
 import 'animate.css';
@@ -10,5 +11,7 @@ const app = createApp(App)
 const pinia = createPinia()
 app.use(router)
 app.use(pinia)
+const counterStore = useCounterStore()
+  console.log('main.js',counterStore.count)
 app.component('BaseIcon', BaseIcon)
 app.mount('#app')
